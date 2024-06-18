ce8785ee8e35b04d99cd94494f76cd488108db70
rishti-rr <rishti18cse.pstu.ac.bd>
2024-06-18 14:57:07 +0600

    bolbona

---
+++ A_Soft_Drinking.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    int x = k*l/nl;
+    int y = c*d;
+    int z = p/np;
+    cout<<min(min(x,y),z)/n<<endl;
+    return 0;
+}
